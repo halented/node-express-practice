7bc47e0f24c54a0ec516a352c3472e3b4bdb4c30
Hal Dunn <heyhal19@gmail.com>
2021-01-12 13:36:44 -0800

    implements leads route, show all leads after submission

--- controllers/landing.js
+++ controllers/landing.js
@@ -3,13 +3,20 @@ const models = require('../models')
 exports.get_landing = function (req, res, next) {
     res.render('landing', { title: 'Slammo' });
 }
+
 exports.submit = function (req, res, next) {
     console.log("whats this: ", req.body.lead_email)
 
-    // create is a sequelize method
+    // create is a sequelize method, can infer that it operates with promises here
     return models.Lead.create({
         email: req.body.lead_email
     }).then(lead => {
-        res.redirect('/')
+        res.redirect('/leads')
     })
+}
+
+exports.show_leads = function (req, res, next) {
+    // we will need to fetch from the database here. findAll is  a sequelize method
+    models.Lead.findAll()
+        .then(data => res.render('landing', { leads: data, title: "Hockeystick"}))
 }
