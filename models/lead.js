7c199375d70dad90f85deb55da920802c6636845
Hal Dunn <heyhal19@gmail.com>
2021-01-12 12:00:50 -0800

    changes config back to json file; adds Lead model file and updates www to use models

--- models/lead.js
+++ models/lead.js
@@ -1,1 +1,19 @@
 // we are collecting emails, which he calls leads, so the db table and model will be called Lead
+
+'use-strict';
+
+module.exports = (sequelize, DataTypes) => {
+    var Lead = sequelize.define('Lead', {
+        id: {
+            tpye: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false,
+            primaryKey: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    })
+    return Lead
+}
